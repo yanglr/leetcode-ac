713e6c53f901eebe7b089109d7af1dde6ee5c070
Bravo <1530676929@qq.com>
2021-07-10 09:42:04 +0800

    Update project information.

--- csharp-leetcode/vs projects/LeetCode65-valid-number/LeetCode65-Valid_Number/Properties/AssemblyInfo.cs
+++ csharp-leetcode/vs projects/LeetCode65-valid-number/LeetCode65-Valid_Number/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LeetCode65-Valid_Number")]
+[assembly: AssemblyTitle("leetcode65-valid-number")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LeetCode65-Valid_Number")]
+[assembly: AssemblyProduct("leetcode65-valid-number")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
