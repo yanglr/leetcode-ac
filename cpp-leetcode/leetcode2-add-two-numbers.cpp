68fc1650cd4a8b0c3a4a5c45abd8a2c0088a38c3
Bravo <1530676929@qq.com>
2021-07-14 21:41:25 +0800

    Fix typo.

--- cpp-leetcode/leetcode2-add-two-numbers.cpp
+++ cpp-leetcode/leetcode2-add-two-numbers.cpp
@@ -19,19 +19,19 @@ public:
         ListNode* fakeNode = new ListNode(-1);
         ListNode* p = fakeNode;
 
-        int carrry = 0;
+        int carry = 0;
         while(l1 || l2)
         {
             int val1 = (l1 != NULL) ? l1 -> val : 0;
             int val2 = (l2 != NULL) ? l2 -> val : 0;
-            int sum = val1 + val2 + carrry;
-            carrry = sum / 10;  // 拿掉这个sum(二位数)的末尾数字, 得到当前位的进位值
+            int sum = val1 + val2 + carry;
+            carry = sum / 10;  // 拿掉这个sum(二位数)的末尾数字, 得到当前位的进位值
             p -> next = new ListNode(sum % 10);
             p = p -> next;
             if(l1) l1 = l1 -> next;
             if(l2) l2 = l2 -> next;
         }
-        if (carrry == 1)
+        if (carry == 1)
             p -> next = new ListNode(1);
 
         return fakeNode -> next;
