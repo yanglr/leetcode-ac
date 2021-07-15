c59f45ce35bc963a875bdad91b1fcdd6d2b62d79
Bravo <1530676929@qq.com>
2021-07-15 21:23:37 +0800

    Add solution for leetcode 20 - 有效的括号.

---
+++ cpp-leetcode/leetcode20-valid-parentheses.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<stack>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(auto ch : s)
+        {
+            if(ch == '(' || ch == '[' || ch == '{')
+                st.push(ch);
+            else {
+                if(st.empty())
+                    return false;
+                if(ch == ')' && st.top() != '(')
+                    return false;
+                if(ch == ']' && st.top() != '[')
+                    return false;
+                if(ch == '}' && st.top() != '{')
+                    return false;
+
+                st.pop();                                   
+            }
+        }
+        return st.empty();
+    }
+};
+
+//Test
+int main()
+{
+    Solution sol;
+    string str = "()[]{}";  // "(}{)"; 
+    bool res = sol.isValid(str);
+    cout << (res == true ? "true" : "false") << endl;
+
+    return 0;
+}
