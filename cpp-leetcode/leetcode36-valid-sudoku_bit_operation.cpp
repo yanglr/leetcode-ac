138d5ae202c4bf0e73e2b4c4d17a187079b96747
Bravo <1530676929@qq.com>
2021-07-17 23:57:43 +0800

    Update.

--- cpp-leetcode/leetcode36-valid-sudoku_bit_operation.cpp
+++ cpp-leetcode/leetcode36-valid-sudoku_bit_operation.cpp
@@ -10,7 +10,7 @@ public:
 		vector<int> col(9);
 		vector<int> boxes(9);
 
-		int shiftCount = 0;
+		int shiftInt = 0;
 		for (int i = 0; i < 9; i++)
 		{
 			for (int j = 0; j < 9; j++)
@@ -18,19 +18,19 @@ public:
 				if (board[i][j] == '.')
 					continue;
 
-				shiftCount = 1 << (board[i][j] - '0');  // 转为二进制，移位结束后目标位为1，其他位均为0
+				shiftInt = 1 << (board[i][j] - '0');  // 转为二进制，移位结束后目标位为1，其他位均为0
 				/* 每个格子若有数字，必为 1 ~ 9，转换成对应的二进制数，1 → 0001，2 → 0010，3→0100，4→1000 ... 该方法适用于 遍历行/遍历列/遍历box  */			
 				int boxPos = (i / 3) * 3 + j / 3; //将大数独棋盘分成9个小棋盘，编号0~8
 
-				// 如果当前数字shiftCount在row[j] 或col[i] 或 boxes中已经存在，&运算后不为0，
+				// 如果当前数字shiftInt在row[j] 或col[i] 或 boxes中已经存在，&运算后不为0，
 				// 只有当前数字没出现过，&运算后为0
-				if ((col[i] & shiftCount) != 0 || (row[j] & shiftCount) != 0 || (boxes[boxPos] & shiftCount) != 0)
+				if ((col[i] & shiftInt) != 0 || (row[j] & shiftInt) != 0 || (boxes[boxPos] & shiftInt) != 0)
 					return false;
 
 				//第 n 位代表 n 这个数字是否存在(1→存在， 0→不存在)，同理于col[i]  boxes[boxPos]
-				row[j] |= shiftCount;
-				col[i] |= shiftCount;
-				boxes[boxPos] |= shiftCount;
+				row[j] |= shiftInt;
+				col[i] |= shiftInt;
+				boxes[boxPos] |= shiftInt;
 			}
 		}
 		return true;
