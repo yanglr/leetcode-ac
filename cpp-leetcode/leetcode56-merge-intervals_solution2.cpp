916f22d524ed1138575f6c7392f039303e7ca821
Bruce Yang <Bruce.Yang@kla-tencor.com>
2021-07-29 12:47:50 +0800

    Optimize code.

--- cpp-leetcode/leetcode56-merge-intervals_solution2.cpp
+++ cpp-leetcode/leetcode56-merge-intervals_solution2.cpp
@@ -9,16 +9,17 @@ public:
         if (intervals.empty())
             return {};
         vector<vector<int>> res;
-        vector<pair<int, int>> q;    /* 扫描线算法: time, flag. 先自定义排序
-                                     **           进来一个start, 计数器+1, 
-                                     **           进来一个end, 计数器-1.
-                                     **           计数器出现0 -> 1的转折点, 表示新的 start 进来了
-                                     **           计数器出现1 -> 0的转折点, 表示所有 []括号成对, merge 结束   
-                                     */
+        vector<pair<int, int>> ranges; /* ranges 是 intervals 的另一种存储方式
+                                       ** 扫描线算法: time, flag. 先自定义排序
+                                       **           进来一个start, 计数器+1, 
+                                       **           进来一个end, 计数器-1.
+                                       **           计数器出现0 -> 1的转折点, 表示新的 start 进来了
+                                       **           计数器出现1 -> 0的转折点, 表示所有 []括号成对, merge 结束   
+                                       */
         for (auto &vect : intervals)
         {
-            q.push_back({vect.front(), 1});
-            q.push_back({vect.back(), -1});
+            ranges.push_back({vect.front(), 1});
+            ranges.push_back({vect.back(), -1});
         }        
 
         auto cmp = [](pair<int, int>& range1, pair<int, int>& range2)
@@ -27,18 +28,18 @@ public:
                 return range1.second > range2.second;
             return range1.first < range2.first;
         };
-        sort(q.begin(), q.end(), cmp);
+        sort(ranges.begin(), ranges.end(), cmp);
 
         int sumCounter = 0;
         int start, end;
-        for (auto x : q)
+        for (auto range : ranges)
         {
-            if (x.second == 1)
+            if (range.second == 1)
             {
                 sumCounter++;
                 if (sumCounter == 1)  // 转折点: 0 -> 1, 新的start进来了
                 {
-                    start = x.first;
+                    start = range.first;
                 }                
             }
             else  /* -1 */
@@ -46,7 +47,7 @@ public:
                 sumCounter--;
                 if (sumCounter == 0)  // 转折点: 1 -> 0, merge结束, 记录结束时的区间右边界end
                 {
-                    end = x.first;
+                    end = range.first;
                     res.push_back({start, end});
                 }                
             }            
