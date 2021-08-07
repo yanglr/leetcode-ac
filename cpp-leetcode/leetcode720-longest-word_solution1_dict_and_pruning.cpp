62453c70d16ecdb2812a5d84309867ad2324371a
Bravo <1530676929@qq.com>
2021-08-07 10:22:26 +0800

    Add C++ solution for leetcode 720.

---
+++ cpp-leetcode/leetcode720-longest-word_solution1_dict_and_pruning.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    string longestWord(vector<string>& words) {
+        if (words.empty()) return "";
+        
+        string best = "";  // best表示当前最长目标词
+        unordered_set<string> dict(words.begin(), words.end());  // 取出所有的key 放进 set
+
+        for (auto& word : words)
+        {
+            if ((word.size() < best.size()) || (word.size() == best.size() && word > best))
+                continue;       /* 剪枝 pruning */
+            
+            bool isValid = true;
+            string prefix;
+            for (int i = 0; i < word.length() - 1 && isValid; i++)
+            {
+                prefix.push_back(word[i]);
+                if (dict.count(prefix) == 0)
+                    isValid = false;                
+            }
+            if (isValid)
+                best = word;            
+        }
+        
+        return best;
+    }
+};
+
+// Test
+int main()
+{
+    Solution sol;
+    vector<string> words = { "a", "banana", "app", "appl", "ap", "apply", "apple" };
+    // vector<string> words = { "yo","ew","fc","zrc","yodn","fcm","qm","qmo","fcmz","z","ewq","yod","ewqz","y" };
+    // vector<string> words = { "r","kt","jtgt","j","jtg","rdwy","chkext","c","l","zo","lnp","k","jt","chke","ktui","rd","jtgtha","ch","chkex" };
+    // vector<string> words = {"b", "br", "bre", "brea", "break", "breakf", "breakfa", "breakfas", "breakfast", "l", "lu", "lun", "lunc", "lunch", "d", "di", "din", "dinn", "dinne", "dinner"};
+    auto res = sol.longestWord(words);
+    cout << res << endl;
+    
+    return 0;
+}
