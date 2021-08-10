795d6a07e59711ca3ec98ac03897d2f5636058b9
Bruce Yang <Bruce.Yang@kla-tencor.com>
2021-08-10 16:42:20 +0800

    Update code comments.

--- cpp-leetcode/leetcode5-longest-palindromic-substring_back_tracking.cpp
+++ cpp-leetcode/leetcode5-longest-palindromic-substring_back_tracking.cpp
@@ -12,15 +12,15 @@ public:
         if (s.empty() || s.size() == 1) return s;        
         for (int i = 0; i < s.size(); i++)
         {
-            backTrack(s, i, i);      /* 原串长度为奇数时, 回溯back tracking开始 */
-            backTrack(s, i, i + 1);  /* 原串长度为偶数时, 回溯开始 */
+            backTrack(s, i, i);      /* 回溯back tracking开始, 寻找长度为奇数的串  */
+            backTrack(s, i, i + 1);  /* 回溯开始, 寻找长度为偶数的串 */
         }
         return s.substr(start, maxLen);
     }
 
     void backTrack(string s, int left, int right) {
         while(left >= 0 && right < s.length() && s[left] == s[right]) {
-            if(right - left + 1 > maxLen) {  /* 发现新的更长回文串时, 更新全局变量startPos和maxLen */
+            if(right - left + 1 > maxLen) {  /* 发现新的更长的回文串时, 更新全局变量startPos和maxLen */
                 start = left;
                 maxLen = right - left + 1;
             }
