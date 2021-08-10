02f545b29c8fc1c04bb8c91ec3799ee025bb2807
Bravo <1530676929@qq.com>
2021-08-10 20:33:01 +0800

    Update code for leetcode 78 - subsets.

--- csharp-leetcode/vs projects/leetcode78-subsets/leetcode78/Program.cs
+++ csharp-leetcode/vs projects/leetcode78-subsets/leetcode78/Program.cs
@@ -1,66 +1,35 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
-namespace leetcode78
-{
-    //public class Solution
-    //{
-    //    static void Main(string[] args)
-    //    {
-    //        var sol = new Solution();
-    //        var res = sol.Subsets(new int[] { 1, 2, 3 });
-    //    }
-
-    //    public IList<IList<int>> Subsets(int[] nums)
-    //    {
-    //        double count = Math.Pow(2, nums.Length);
-
-    //        IList<IList<int>> res = new List<IList<int>>();
-    //        res.Add(new List<int>());
-    //        for (int i = 1; i <= count - 1; i++)
-    //        {
-    //            var lst = new List<int>();
-    //            string str = Convert.ToString(i, 2).PadLeft(nums.Length, '0');
-    //            for (int j = 0; j < str.Length; j++)
-    //            {
-    //                if (str[j] == '1')
-    //                {
-    //                    lst.Add(nums[j]);
-    //                }
-    //            }
-    //            res.Add(lst);
-    //        }
-
-    //        return res;
-    //    }
-    //}
-
-    public class Solution
-    {
-        public bool JudgeSquareSum(int c)
-        {
-            var r = (int)Math.Sqrt(c);
-            var l = 0;
-            while (l <= r)
-            {
-                var sum = l * l + r * r;
-                if (sum == c)
-                    return true;
-
-                if (sum < c)
-                    l++;
-                else
-                    r--;
+namespace leetcode78 {
+    public class Solution {
+        public IList<IList<int>> Subsets(int[] nums) {
+            double count = Math.Pow(2, nums.Length);    /* 大的思路: 转为二进制(长度与原数组中数字个数相等)的字符串, 然后枚举. 由于各个元素中不存在相等的数，于是每个二进制数位都有出现/不出现(0, 1)两种状态, 共有2^n组  */
+            IList<IList<int>> res = new List<IList<int>>();
+            res.Add(new List<int>());
+            for (int i = 1; i < count; i++) {
+                var curSet = new List<int> ();
+                string str = Convert.ToString(i, 2).PadLeft(nums.Length, '0');  /* Convert.ToString(i, 2)表示转为二进制的字符串, PadLeft表示如果位数不够会在左侧填补上字符'0' */
+                for (int j = 0; j < str.Length; j++)
+                {
+                    if (str[j] == '1')
+                        curSet.Add(nums[j]);
+                }
+                res.Add(curSet);
             }
 
-            return false;
+            return res;
         }
 
-        static void Main(string[] args)
-        {
-            var sol = new Solution();
-            var res = sol.JudgeSquareSum(25);
-            Console.WriteLine(res);
+        static void Main (string[] args) {
+            var sol = new Solution ();
+            var res = sol.Subsets(new int[] { 1, 2, 3 });
+            foreach (var theSet in res)
+            {
+                theSet.ToList().ForEach(x => Console.Write(x + ","));
+                Console.WriteLine();
+            }
         }
     }
 }
