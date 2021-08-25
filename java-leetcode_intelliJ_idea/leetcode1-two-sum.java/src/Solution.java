f812cd2bdc64c394e231c9e4e6d5783b20b17de0
Bruce Yang <Bruce.Yang@kla-tencor.com>
2021-08-25 14:04:15 +0800

    Update print method.

--- java-leetcode_intelliJ_idea/leetcode1-two-sum.java/src/Solution.java
+++ java-leetcode_intelliJ_idea/leetcode1-two-sum.java/src/Solution.java
@@ -27,6 +27,6 @@ class Solution {
         int[] nums = {2,7,11,15};
         int target = 9;
         int[] res = sol.twoSum(nums, target);
-        System.out.println(res);
+        System.out.println(Arrays.toString(res));
     }
 }
