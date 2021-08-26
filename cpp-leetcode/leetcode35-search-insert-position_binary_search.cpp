5a36bc79229cfeb0944c185185052969ce184699
Bruce Yang <Bruce.Yang@kla-tencor.com>
2021-08-26 17:18:30 +0800

    Clean code.

--- cpp-leetcode/leetcode35-search-insert-position_binary_search.cpp
+++ cpp-leetcode/leetcode35-search-insert-position_binary_search.cpp
@@ -6,12 +6,12 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         int L = 0, R = nums.size() - 1;
-        while (L < R) {
+        while (L < R)
+        {
             int M = (L + R) / 2;
-            if (nums[M] < target) {
+            if (nums[M] < target)
                 L = M + 1;
-            } else 
-                R = M;
+            else R = M;
         }
         return (nums[L] < target) ? L + 1 : L;
     }
---
+++ cpp-leetcode/leetcode35-search-insert-postion_solution1.cpp
@@ -0,0 +1,41 @@
+#include<cstdio>
+#include<vector>
+#include <algorithm>
+using namespace std;
+
+/* 基于find库函数来做, 分两种情况处理 */
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {        
+        int len = nums.size();
+        int insertPos = -1;
+        auto it = find(nums.begin(), nums.end(), target);
+        if (it != nums.end())
+        {
+            insertPos = it - nums.begin();
+            return insertPos;           
+        }
+        else
+        {
+            if (target < nums[0]) return 0;
+            if (target > nums[len-1]) return len;
+            for (int i = 1; i < len; i++)
+            {
+                if (target > nums[i-1] && target < nums[i]) /* 题意: 数组中不会出现元素相等的情况 */
+                    insertPos = i;
+            }
+        }        
+        return insertPos;
+    }
+};
+
+// Test
+int main()
+{
+    Solution sol;
+    vector<int> nums = {1,3,5,6};
+    auto target = 5;
+    printf("%d", sol.searchInsert(nums, target));
+
+    return 0;
+}
