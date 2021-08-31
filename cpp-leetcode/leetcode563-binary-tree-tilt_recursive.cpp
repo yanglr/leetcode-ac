eda60506445fad1642b6821e2f959c7b89eda894
Bruce Yang <Bruce.Yang@kla-tencor.com>
2021-08-31 15:23:29 +0800

    Add C++ solution for leetcode 563.

---
+++ cpp-leetcode/leetcode563-binary-tree-tilt_recursive.cpp
@@ -0,0 +1,50 @@
+#include<algorithm>
+#include<iostream>
+using namespace std;
+/**
+ * Definition for a binary tree node.
+*/ 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int findTilt(TreeNode* root) {
+        int tiltSum = 0;
+        dfs(root, tiltSum);
+        return tiltSum;
+    }
+    // dfs: Sum all child node values of current node
+    int dfs(TreeNode* root, int& tiltSum)
+    {
+        if (root == nullptr) return 0;
+        int leftValSum = dfs(root->left, tiltSum);
+        int rightValSum = dfs(root->right, tiltSum);
+        int curNodeTilt = abs(leftValSum - rightValSum);
+        tiltSum += curNodeTilt;
+        
+        return leftValSum + rightValSum + root->val;
+    }
+};
+
+// Test
+int main()
+{
+    Solution sol;
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->right->left = new TreeNode(4);
+    root->right->right = nullptr;
+
+    auto res = sol.findTilt(root);   
+    cout << res << endl;
+
+    return 0;
+}
