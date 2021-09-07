d83fa809e56d0ba72450d99b8911669f7f361483
Bruce Yang <Bruce.Yang@kla-tencor.com>
2021-09-07 17:49:11 +0800

    Add code comments.

--- cpp-leetcode/leetcode200-number-of-islands_flood_fill.cpp
+++ cpp-leetcode/leetcode200-number-of-islands_flood_fill.cpp
@@ -23,19 +23,19 @@ public:
         return count;            
     }
 
-    int dfs(int r, int c)
+    int dfs(int r, int c) /* dfs: return能形成的连通岛屿的数量(上下左右四连通) */
     {    
         if (r < 0 || c < 0 || r == rows || c == cols)
             return 0;
         if (M[r][c] == '0')
             return 0;
         
-        M[r][c] = '0';
+        M[r][c] = '0';  /* 将当前格子标记为: visited, 并检查当前格子的上下左右4个位置。下次遇到1(地)就继续向前, 下次遇到0(水, 表示已到尽头)就结束  */
 
-        dfs(r - 1, c);  
-        dfs(r + 1, c);  
-        dfs(r, c - 1);  
-        dfs(r, c + 1); 
+        dfs(r - 1, c);  // 选择上方相邻位置前进
+        dfs(r + 1, c);  // 选择下方相邻位置前进
+        dfs(r, c - 1);  // 选择左侧相邻位置前进
+        dfs(r, c + 1);  // 选择右侧相邻位置前进
 
         return 1; 
     }
