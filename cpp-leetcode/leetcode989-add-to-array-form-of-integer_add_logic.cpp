9c2ec5c6e674878de27b8c7c6b4020233ae932b0
Bruce Yang <Bruce.Yang@kla-tencor.com>
2021-09-10 15:04:11 +0800

    Add code comments.

--- cpp-leetcode/leetcode989-add-to-array-form-of-integer_add_logic.cpp
+++ cpp-leetcode/leetcode989-add-to-array-form-of-integer_add_logic.cpp
@@ -6,28 +6,24 @@ using namespace std;
 class Solution {
 public:
     vector<int> addToArrayForm(vector<int>& A, int k) {
-        if (k == 0) return A;
+        if (k == 0) return A; /* 题目中k的范围是 k >= 0, 为0时不需要计算, 直接返回A */
+        const int len = A.size();
         vector<int> res;
-        int n = A.size();
-        // 对位相加
+        // 数组A与数k的末位对齐, 对位相加, 记录进位
         int carry = 0;
-        int sum = 0;      
-        int i = n - 1; 
+        int i = len - 1; // i: 数组A中从后往前的指针
         while (k > 0 || i >= 0)
         {
-            sum = carry + (k % 10);
-            if (i >= 0) // 保证访问A[i]前不越界
-                sum += A[i];
+            int sum = (k % 10) + carry;
+            sum += (i >= 0 ? A[i] : 0);  // 保证访问A[i]前不越界
+            res.push_back(sum % 10);
+            carry = sum / 10; // 或写成 carry = (sum <= 9) ? 0 : 1;
+            k /= 10; // 删掉数k的最后一位, 继续进行下一轮迭代
 
-            carry = (sum <= 9) ? 0 : 1;
-            res.push_back(sum % 10);            
-            k = k / 10;
-            
             i--;
         }
-        if (carry == 1) res.push_back(1);
+        if (carry == 1) res.push_back(1); /* 如果最高位产生进位, 需要在最后增加一位, 值为1 */
         reverse(res.begin(), res.end());
-        
         return res;
     }
 };
