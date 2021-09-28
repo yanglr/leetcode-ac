32cff9c33a7a24dc2a7c48314dd5e84305c5850d
Bruce Yang <Bruce.Yang@kla-tencor.com>
2021-09-28 15:39:20 +0800

    Clean.

--- cpp-leetcode/test.cpp
+++ cpp-leetcode/test.cpp
@@ -1,19 +1,7 @@
 #include<vector>
-#include<array>
 #include<algorithm>
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-class Solution {
-public:
-    int getSum(int a, int b) {
-        unsigned int carry;
-        while (b != 0) /* 把b先拿过去加一次, 后面用b存储每一次的进位值 */
-        {
-            carry = (a & b) << 1; /* 单独处理进位 */
-            a ^= b;               /* 对位相加, 不处理进位的部分 */
-            b = carry;
-        }
-        return a;
-    }
-};
+
