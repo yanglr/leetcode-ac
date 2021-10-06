df430dba4afe9e88e462b2f32fb7fd5813d82986
Bravo <1530676929@qq.com>
2021-10-06 21:28:54 +0800

    Rename and clean code.

--- cpp-leetcode/test.cpp
+++ cpp-leetcode/test.cpp
@@ -4,24 +4,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Solution {
-    const int N = 1e5;
-public:
-    int distributeCandies(vector<int>& candyType) {
-        bool flags[2 * N + 1]; /* 标记当前num是否是重复出现, -10^5 <= candyType[i] <= 10^5 */
-        fill(flags, flags + 2 * N + 1, false);
-        int count = 0;
-        int half = candyType.size() / 2;
-        for (auto &candy : candyType)
-        {
-            candy += N;
-            if (!flags[candy])
-            {
-                if (++count >= half)
-                    return count;
-                flags[candy] = true;
-            }
-        }
-        return count;
-    }
-};
+// Test
+int main()
+{
+    // Solution sol;
+    vector<int> nums = {1, 3, 2, 2, 5, 2, 3, 7};
+    // auto res = sol.findLHS(nums);
+    // cout << res << endl;
+
+    return 0;
+}
