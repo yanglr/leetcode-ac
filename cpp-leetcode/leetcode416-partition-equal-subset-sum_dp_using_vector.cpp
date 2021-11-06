9f7f6655f9688b15a5ff4a51cc21ccab326e900a
Bravo <yangle20081982@163.com>
2021-11-06 17:49:41 +0800

    Add C++ solutions for leetcode 416.

---
+++ cpp-leetcode/leetcode416-partition-equal-subset-sum_dp_using_vector.cpp
@@ -0,0 +1,36 @@
+#include<vector>
+#include<algorithm>
+#include<numeric>
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum0 = accumulate(nums.begin(), nums.end(), 0);
+        if (sum0 % 2 != 0) return false;
+        int sum = sum0/2;
+        vector<bool> dp(sum + 1); // dp[i]: 原数组nums中的元素是否能凑出和为i, 如果能就将dp值更新为1。每来一个数nums[i], 都有选或不选两种状态
+        dp[0] = true;
+        for (int& num : nums)
+        {
+            for (int j = sum; j >= 0; j--)
+            {
+                if (j >= num) /* 此处必须带上= */
+                    dp[j] = dp[j] || dp[j - num];
+            }            
+        }
+        return dp[sum];                
+    }
+};
+
+// Test
+int main()
+{
+    Solution sol;
+    vector<int> nums = {1, 5, 11, 5};
+    auto res = sol.canPartition(nums);
+    cout << (res ? "True" : "False") << endl;
+
+    return 0;
+}
