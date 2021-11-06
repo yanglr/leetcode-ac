fe2f12f5e69bf24389193422c009b60edb0b3f5c
Bravo <yangle20081982@163.com>
2021-11-06 12:39:47 +0800

    Add C++ solutions for leetcode 837.

---
+++ cpp-leetcode/leetcode837-new-21-game_dp_and_sliding_window_using_vector.cpp
@@ -0,0 +1,39 @@
+#include<vector>
+#include<algorithm>
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    double new21Game(int N, int K, int W) {
+        if (K == 0) return 1.0;        
+        if (N >= K + W - 1) return 1.0;
+        if (N < K) return 0.0;
+        // dp[i] = (1/w) * (dp[i - 1] + ... + dp[i - w])
+        vector<double> dp(K + W); // 最大能达到的点数是 K+W-1
+        double prob = 1.0 / W;
+        dp[0] = 1.0;
+        double sum = 1.0;
+        for (int i = 1; i < K + W; i++)
+        {
+            dp[i] = prob * sum;
+            if (i < K) sum += dp[i];
+            if (i - W >= 0) sum -= dp[i - W];
+        }
+        double res = 0.0;
+        for (int i = K; i <= N; i++)
+            res += dp[i];
+        return res;
+    }
+};
+
+// Test
+int main()
+{
+    Solution sol;
+    int N = 21, K = 17, W = 10;
+    auto res = sol.new21Game(N, K, W);
+    cout << res << endl;
+
+    return 0;
+}
