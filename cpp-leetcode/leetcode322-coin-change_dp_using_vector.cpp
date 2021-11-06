4364f07b52e67f67ceac7516f86bfb180661c977
Bravo <yangle20081982@163.com>
2021-11-06 10:24:42 +0800

    Add C++ solutions for leetcode 322.

---
+++ cpp-leetcode/leetcode322-coin-change_dp_using_vector.cpp
@@ -0,0 +1,33 @@
+#include<vector>
+#include<algorithm>
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, 10001); // dp[i]: 凑成金额 i 所需的 最少的硬币个数, 且数据范围0 <= amount <= 10^4
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) /* 填充 dp[1] ~ dp[amount] */
+        {
+            for (auto& coin : coins)
+            {
+                if (i < coin) continue; // 当前面值大于要凑的总额
+                dp[i] = min(dp[i], dp[i - coin] + 1);
+            }
+        }
+        return dp[amount] == 10001 ? -1 : dp[amount];
+    }
+};
+
+// Test
+int main()
+{
+    Solution sol;
+    vector<int> coins = {1, 2, 5};
+    int amount = 11;
+    auto res = sol.coinChange(coins, amount);
+    cout << res << endl;
+
+    return 0;
+}
